58e5ae7180c71e5c36dda3d6d1c49270bcd55aa3
Hell <617494454@qq.com>
2019-09-30 09:56:01 +0800

    init library

---
+++ HETimer.cpp
@@ -0,0 +1,106 @@
+﻿#include "HETimer.h"
+
+
+using namespace std;
+
+
+HETimer::HETimer() {
+	bus = nullptr;
+	InitializeCriticalSection(&critical_section);
+}
+
+HETimer::~HETimer() {
+	EnterCriticalSection(&critical_section);
+	task.clear();
+	LeaveCriticalSection(&critical_section);
+	if (bus != nullptr) {
+		bus->join();
+		delete bus;
+		bus = nullptr;
+	}
+}
+
+void HETimer::AddTimer(unsigned int id, unsigned long usec, int param, const char* str_param) {
+	if (usec > 0) {
+		HETimerInfo info;
+		info.id = id;
+		info.usec = usec;
+		info.param = param;
+		info.str_param = str_param;
+		info.last_time = GetTickCount();
+		info.del = false;
+		EnterCriticalSection(&critical_section);
+		task.push_back(info);
+		LeaveCriticalSection(&critical_section);
+		if (task.size() == 1) {
+			if (bus != nullptr) {
+				bus->join();
+				delete bus;
+				bus = nullptr;
+			}
+			bus = new thread{ &HETimer::OnThread, this };
+		}
+	}
+}
+
+void HETimer::CancelTimer(unsigned int id) {
+	EnterCriticalSection(&critical_section);
+	for (auto item = task.begin(); item != task.end(); ++item) {
+		if (item->id == id) {
+			item->del = true;
+			LeaveCriticalSection(&critical_section);
+			return;
+		}
+	}
+	LeaveCriticalSection(&critical_section);
+}
+
+void HETimer::CloseTimer() {
+	EnterCriticalSection(&critical_section);
+	task.clear();
+	LeaveCriticalSection(&critical_section);
+}
+
+void HETimer::TimerRun() {
+	const auto now = GetTickCount();
+	EnterCriticalSection(&critical_section);
+	for (auto item = task.begin(); item != task.end(); ++item) {
+		if (now - item->last_time >= item->usec) {
+			item->last_time = now;
+			if (OnTimer(item->id, item->param, item->str_param) == -1) {
+				item->del = true;
+			}
+		}
+	}
+	LeaveCriticalSection(&critical_section);
+}
+
+
+void HETimer::TimerDelete() {
+	EnterCriticalSection(&critical_section);
+	for (auto item = task.begin(); item != task.end();) {
+		if (item->del) {
+			task.erase(item);
+			item = task.begin();
+			continue;
+		}
+		++item;
+	}
+	LeaveCriticalSection(&critical_section);
+}
+
+auto HETimer::ThreadIsClose() const -> bool {
+	return task.empty();
+}
+
+
+void HETimer::OnThread() {
+	while (true) {
+		if (ThreadIsClose()) {
+			break;
+		}
+		TimerDelete();
+		TimerRun();
+	}
+}
+
---
+++ HETimer.h
@@ -0,0 +1,64 @@
+#ifndef HETIMER_H
+#define HETIMER_H
+
+#include <Windows.h>
+#include <list>
+#include <thread>
+#include <consoleapi.h>
+
+struct HETimerInfo {
+	unsigned int id;
+	unsigned long usec;
+	unsigned long last_time;
+	int param;
+	const char* str_param;
+	bool del;
+	HETimerInfo() {
+		id = 0;
+		usec = 0;
+		last_time = 0;
+		param = 0;
+		str_param = nullptr;
+		del = false;
+	}
+};
+
+typedef std::list<HETimerInfo> TimerList;
+typedef std::list<HETimerInfo>::iterator TimerListItem;
+
+
+
+
+
+class HETimer {
+public:
+	HETimer();
+	virtual ~HETimer();
+	
+	//添加一个定时任务
+	void AddTimer(unsigned int id, unsigned long usec, int param = 0, const char* str_param = nullptr);
+	//关闭一个定时任务
+	void CancelTimer(unsigned int id);
+
+public:
+	virtual int OnTimer(int id, int param, const char *str_param) = 0;
+	
+protected:
+	void CloseTimer();
+
+private:
+	auto ThreadIsClose() const -> bool;
+	void TimerDelete();
+	void TimerRun();
+	void OnThread();
+	
+private:
+	std::thread *bus;
+	bool is_close = false;
+	TimerList task;
+	CRITICAL_SECTION critical_section;
+};
+
+
+
+#endif
